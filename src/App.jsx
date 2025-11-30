ccdf16d30dd9e9d8e15f7ba75ebb7ad99446ad32
gozuray <ray.peratta.e@gmail.com>
2025-11-30 12:19:57 +0000

    Initial ETH Wallet Inspector setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>ETH WALLET INSPECTOR</h1>
+      <p>Connect your wallet to inspect your Ethereum assets.</p>
+      <button>Connect Wallet</button>
+    </div>
+  );
+}
+
+export default App;
